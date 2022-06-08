db053b6be6087f8e703650c0a63636d60a1127c6
Max McDonough <max.mcdonough@gmail.com>
2022-06-08 12:14:46 -0400

    Basic map layer displayed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,25 @@
 let userLat;
 let userLon;
-
+let map;
+let gotPosition = false;
 
 // Get user's location by lat long
 let positionSuccess = (position) => {
     userLat = position.coords.latitude;
     userLon = position.coords.longitude;
+    generateMap(userLat, userLon); 
     console.log(`User's position is: Lat: ${userLat}, Lon: ${userLon}`);
 };
 let positionError = (err) => {
     console.log(err.code);
 };
 navigator.geolocation.getCurrentPosition(positionSuccess, positionError);
+
+// Generate map function
+let generateMap = (userLat, userLon) => {
+    map = L.map('map').setView([userLat, userLon], 13);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '© OpenStreetMap'
+    }).addTo(map);
+};
