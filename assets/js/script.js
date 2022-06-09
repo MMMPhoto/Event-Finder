03a4713cfde97781e98850ac769b52b03bebcbb1
Taylor Guenthner <wtguenthner@gmail.com>
2022-06-09 08:36:26 -0400

    Date select working

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,9 +6,12 @@ function handleSearch() {
     var city = document.querySelector('#cityInput').value;
     var genre = document.querySelector('#genreInput').value;
     var family = document.querySelector('#familyInput').value;
-    var date = document.querySelector('#dateInput').value;
+    var day = document.querySelector('#dayInput').value;
+    var year = document.querySelector('#yearInput').value;
+    var month = document.querySelector('#monthInput').value;
+    // var date = document.querySelector('#dateInput').value;
 
-    let url = `https://app.ticketmaster.com/discovery/v2/events.json?size=9&city=${city}&classificationName=${genre}&includeFamily=${family}&l&apikey=${apiKey}`
+    let url = `https://app.ticketmaster.com/discovery/v2/events.json?size=9&sort=date,asc&city=${city}&startDateTime=${year}-${month}-${day}T14:00:00Z&classificationName=${genre}&l&apikey=${apiKey}`
 
     fetch(url).then(data => data.json()).then(data => {
 
@@ -19,11 +22,12 @@ function handleSearch() {
 }
 
 function displayEvents(jsonData) {
-   
+    document.getElementById("eventList").style.display = "flex";
     for (var x = 0; x < 9; x++) {
         
         var eventDisplay = document.querySelector(`#event${x+1}`);
         eventDisplay.innerHTML = `<p class="eventDisplay">${jsonData._embedded.events[x].name}</p>`
+        eventDisplay.innerHTML += `<p class="eventDisplay">${jsonData._embedded.events[x].dates.start.localDate}</p>`
         eventDisplay.innerHTML += `<p class="eventImage"><img height="auto" width="200" src="${jsonData._embedded.events[x].images[0].url}"></p>`;
 
     }
