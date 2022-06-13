bf2d20f409758c2aff92f09d61664b70fedc74d1
Taylor Guenthner <wtguenthner@gmail.com>
2022-06-13 18:55:24 -0400

    Style changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -36,26 +36,29 @@ function handleSearch() {
 };
 
 function displayEvents(jsonData) {
+    var eventHeader = document.querySelector('#eventHeader')
+   
     //This element begins as display: none. Changes it to flex when submit button is pressed
     document.getElementById("eventList").style.display = "flex";
 
-    var eventHeader = document.querySelector('#eventHeader')
+    
     var city = document.querySelector('#cityInput').value;
     var genre = document.querySelector('#genreInput').value;
-
+    
     //Changes the event header to the user's city and genre selection
     if (genre == "Musicals") {
         eventHeader.innerHTML = `<u>${city} ${genre}</u>`;
     } else {
         eventHeader.innerHTML = `<u>${city} ${genre} Events</u>`;
     };
-
+   
     for (var x = 0; x < 9; x++) {
 
         //Targets the text area for the corresponding event 
         var eventDisplay = document.querySelector(`#event${x + 1}`);
 
         //Sets array for the date to rearrange it to be in mm/dd/yyyy format
+        
         var arr = jsonData._embedded.events[x].dates.start.localDate.split("-");
         var date = `${arr[1]}-${arr[2]}-${arr[0]}`;
 
@@ -66,8 +69,9 @@ function displayEvents(jsonData) {
         eventDisplay.innerHTML += `<p class="eventImage"><img height="auto" width="200" src="${jsonData._embedded.events[x].images[0].url}"></p><br>`;
         // eventDisplay.innerHTML += `<p class = "eventTickets"><a href=${jsonData._embedded.events[x].url}>Buy Tickets</a></p></button>`;
         eventDisplay.innerHTML += `<span class="setBottom"><span class = "eventTickets"><a href=${jsonData._embedded.events[x].url}>Buy Tickets</a><button class="infoButton" value = "${x}" onclick="displayData(this.value)">Info</button></span></span>`;
-
+        
     };
+
 
 }
 
