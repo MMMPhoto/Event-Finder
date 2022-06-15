fd48f16c28b83d167dfc62a5f1889a9113647518
John Evenden <johnthefightguy@gmail.com>
2022-06-15 10:17:56 -0400

    Finally got pace scrolling to work. Had to change the variables around which the for loops run, and change the html file to dynamically create the boxes instead of hard coding them

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,7 @@
 var apiKey = "TcnFtVJuGcgL3ubSuuGQMpjlZcPwVVqZ";
 var eventSubmit = document.querySelector(".event");
+let x = 0;
+let y = 9;
 
 function handleSearch() {
   var info = document.getElementById("infoDisplay");
@@ -10,44 +12,44 @@ function handleSearch() {
   var year = document.querySelector("#yearInput").value;
   var month = document.querySelector("#monthInput").value;
   var radius = document.querySelector("#radiusInput").value;
-
   // Resets the right side info text area when another event is searched
   info.innerHTML = `<br><u>Event Info</u>`;
 
   // API fetch
-  let url = `https://app.ticketmaster.com/discovery/v2/events.json?size=9&sort=date,asc&city=${city}&radius=${radius}&startDateTime=${year}-${month}-${day}T14:00:00Z&classificationName=${genre}&l&apikey=${apiKey}`;
+  let url = `https://app.ticketmaster.com/discovery/v2/events.json?size=${y}&sort=date,asc&city=${city}&radius=${radius}&startDateTime=${year}-${month}-${day}T14:00:00Z&classificationName=${genre}&l&apikey=${apiKey}`;
 
   fetch(url)
     .then((data) => data.json())
     .then((data) => {
       jsonData = data;
-      displayEvents(jsonData);
+      displayEvents(jsonData, x, y);
       console.log(jsonData);
     });
 }
 
-function displayEvents(jsonData) {
+function displayEvents(jsonData, x, y) {
   //This element begins as display: none. Changes it to flex when submit button is pressed
-  document.getElementById("eventList").style.display = "flex";
-
+  // document.getElementById("eventList").style.display = "flex";
   var eventHeader = document.querySelector("#eventHeader");
   var city = document.querySelector("#cityInput").value;
   var genre = document.querySelector("#genreInput").value;
+  //   var city = document.querySelector("#cityInput").value;
+  //   var genre = document.querySelector("#genreInput").value;
 
-  //Changes the event header to the user's city and genre selection
-  if (genre == "Musicals") {
-    eventHeader.innerHTML = `<u>${city} ${genre}</u>`;
-  } else {
-    eventHeader.innerHTML = `<u>${city} ${genre} Events</u>`;
-  }
+  //Sets array for the date to rearrange it to be in mm/dd/yyyy format
+  var arr = jsonData._embedded.events[x].dates.start.localDate.split("-");
+  var date = `${arr[1]}-${arr[2]}-${arr[0]}`;
 
-  for (var x = 0; x < 9; x++) {
+  eventHeader.innerHTML = `<button onclick="prevPage(jsonData)">Prev</button>     <u>${city} ${genre}</u>     <button onclick="nextPage(jsonData)">Next</button>`;
+
+  let eventList = document.querySelector("#eventList");
+  eventList.innerHTML = "";
+  for (var x; x < y; x++) {
     //Targets the text area for the corresponding event
-    var eventDisplay = document.querySelector(`#event${x + 1}`);
 
-    //Sets array for the date to rearrange it to be in mm/dd/yyyy format
-    var arr = jsonData._embedded.events[x].dates.start.localDate.split("-");
-    var date = `${arr[1]}-${arr[2]}-${arr[0]}`;
+    eventList.innerHTML += `<div class="event" id="event${x + 1}"></div>`;
+
+    var eventDisplay = document.querySelector(`#event${x + 1}`);
 
     // Generates each listed event
     eventDisplay.innerHTML = `<button class="saveButton">Save</button>`;
@@ -57,6 +59,20 @@ function displayEvents(jsonData) {
     // eventDisplay.innerHTML += `<p class = "eventTickets"><a href=${jsonData._embedded.events[x].url}>Buy Tickets</a></p></button>`;
     eventDisplay.innerHTML += `<span class="setBottom"><span class = "eventTickets"><a href=${jsonData._embedded.events[x].url}>Buy Tickets</a><button class="infoButton" value = "${x}" onclick="displayData(this.value)">Info</button></span></span>`;
   }
+}
+
+function nextPage(jsonData) {
+  x = x + 9;
+  y = y + 9;
+  handleSearch(jsonData, x, y);
+  return x, y;
+}
+
+function prevPage(jsonData) {
+  x = x - 9;
+  y = y - 9;
+  handleSearch(jsonData, x, y);
+  return x, y;
 }
 
 // Function for display info on the right side of screen
